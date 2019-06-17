3afdbbcff21f571535d9b4a66f353da372b4a279
DucMinh Ngo <dmngo@umich.edu>
2019-06-17 08:51:38 +0700

    Remove unnecessary file

--- ocrFinal.py
+++
@@ -1,5 +0,0 @@
-from ocrLTest import readText
-from ocrNum import readNum
-
-print("Name:", readText('C:/Users/MinhND34/Desktop/ImageTest/1cmnd.jpg'))
-print("ID:", readNum('C:/Users/MinhND34/Desktop/ImageTest/1cmnd.jpg'))
